8a9d6a3e479e2f5f1283b70aa30ef91d9990dcb5
Bruno Henrique Saibert <b.saibert@vhsys.com.br>
2020-01-22 23:12:17 -0300

    Active link menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,47 +1,75 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import { FaSearch, FaBell, FaUser } from 'react-icons/fa';
 
 import * as S from './styles';
 
 import logo from '../../assets/logo.png';
 
-export default function Header() {
-  return (
-    <S.HeaderSize>
-      <S.Header>
-        <S.Div>
-          <S.Logo src={logo} alt="" />
+class Header extends Component {
+  render() {
+    const {
+      location: { pathname },
+    } = this.props;
 
-          <S.Nav>
-            <S.NavLink>
-              <Link to="/">Início</Link>
-            </S.NavLink>
-            <S.NavLink>
-              <Link to="/">Séries</Link>
-            </S.NavLink>
-            <S.NavLink>
-              <Link to="/">Filmes</Link>
-            </S.NavLink>
-            <S.NavLink>
-              <Link to="/latest">Mais recentes</Link>
-            </S.NavLink>
-            <S.NavLink>
-              <Link to="/my-list" className="active">
-                Minha lista
-              </Link>
-            </S.NavLink>
-          </S.Nav>
-        </S.Div>
+    return (
+      <S.HeaderSize>
+        <S.Header>
+          <S.Div>
+            <S.Logo src={logo} alt="" />
 
-        <S.Div>
-          <S.Actions>
-            <FaSearch />
-            <FaBell />
-            <FaUser />
-          </S.Actions>
-        </S.Div>
-      </S.Header>
-    </S.HeaderSize>
-  );
+            <S.Nav>
+              <S.NavLink>
+                <Link to="/" className={pathname === '/' ? 'active' : null}>
+                  Início
+                </Link>
+              </S.NavLink>
+              <S.NavLink>
+                <Link
+                  to="/series"
+                  className={pathname === '/series' ? 'active' : null}
+                >
+                  Séries
+                </Link>
+              </S.NavLink>
+              <S.NavLink>
+                <Link
+                  to="/movies"
+                  className={pathname === '/movies' ? 'active' : null}
+                >
+                  Filmes
+                </Link>
+              </S.NavLink>
+              <S.NavLink>
+                <Link
+                  to="/latest"
+                  className={pathname === '/latest' ? 'active' : null}
+                >
+                  Mais recentes
+                </Link>
+              </S.NavLink>
+              <S.NavLink>
+                <Link
+                  to="/my-list"
+                  className={pathname === '/my-list' ? 'active' : null}
+                >
+                  Minha lista
+                </Link>
+              </S.NavLink>
+            </S.Nav>
+          </S.Div>
+
+          <S.Div>
+            <S.Actions>
+              <FaSearch />
+              <FaBell />
+              <FaUser />
+            </S.Actions>
+          </S.Div>
+        </S.Header>
+      </S.HeaderSize>
+    );
+  }
 }
+
+export default withRouter(Header);
